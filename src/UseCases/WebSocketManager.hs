cbb006f6f83fa792a9e3d26ab1c6fe03c9cc7b38
Marco Tarantino <taran.marco@protonmail.com>
2021-05-12 14:58:26 +0200

    Extract onOpen handlers for CryptoWatch

--- src/UseCases/WebSocketManager.hs
+++ src/UseCases/WebSocketManager.hs
@@ -1,5 +1,6 @@
 module UseCases.WebSocketManager where
 
+import Control.Concurrent
 import qualified Data.ByteString.Lazy.Char8 as BS
 import qualified Domain.CryptoWatch.WS as CW
 import Domain.Targets
@@ -7,11 +8,25 @@ import Domain.WebSocket
 import qualified Network.WebSockets as WS
 import Polysemy
 
+subscribeAndCloseAfter :: Member (Embed IO) r => BS.ByteString -> Int -> SemClientApp r ()
+subscribeAndCloseAfter subMsg delay conn = do
+    embed $ putStrLn "Sending subscribe message"
+    _ <- embed . forkIO $ do
+        threadDelay delay
+        putStrLn "Sending close message"
+        WS.sendClose conn ("Close" :: BS.ByteString)
+    embed $ WS.sendTextData conn subMsg
+
+subscribeOnOpen :: Member (Embed IO) r => BS.ByteString -> SemClientApp r ()
+subscribeOnOpen subMsg conn = do
+    embed $ putStrLn "Sending subscribe message"
+    embed $ WS.sendTextData conn subMsg
+
 cryptoWatchWSPayload :: Member (Embed IO) r => WSAppConfig r
 cryptoWatchWSPayload = WSAppConfig onOpen onMessage
   where
-    onOpen conn = embed $ WS.sendTextData conn krakenSub
-    krakenSub = CW.subscribePayload ["markets:87:trades"]
+    onOpen = subscribeOnOpen krakenSub
+    krakenSub = CW.subscribePayload ["markets:*:trades"]
     onMessage = embed . BS.putStrLn
 
 makeWSConfig :: Member (Embed IO) r => Exchange -> WSTarget r
